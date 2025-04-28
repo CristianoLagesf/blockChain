bd89773a63e1380ed31c81a67e2c8d35e0e747bd
Cristiano Lages <cristianolagesf@hotmail.com>
2025-04-28 14:42:34 -0400

    updated the Main to call the CLI

--- main.go
+++ main.go
@@ -1,23 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockChain()
-	bc.AddBlock("send 1 BTC to ivan")
-	bc.AddBlock("send 2 more BTC to Ivan")
+	defer bc.db.Close()
+	cli := CLI{bc}
+	cli.Run()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
+	// for _, block := range bc.blocks {
+	// 	fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+	// 	fmt.Printf("Data: %s\n", block.Data)
+	// 	fmt.Printf("Hash: %x\n", block.Hash)
+	// 	fmt.Println()
 
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	// 	pow := NewProofOfWork(block)
+	// 	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+	// 	fmt.Println()
+	// }
 }
